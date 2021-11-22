e1c12f22309160d1d609336d6c641a0548d65e1d
Arvik <ugo.bercegeay@gmail.com>
2021-11-22 21:15:37 +0100

    Ajout MPD et correction du MCD et MLD

---
+++ BaseDeDonnee/KUT-Cinema DB structure.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE IF NOT EXISTS `KUT-Cinema`;
+
+USE `KUT-Cinema`;
+
+DROP TABLE IF EXISTS `Client`;
+CREATE TABLE IF NOT EXISTS `Client` (
+    `id_client` int (11) NOT NULL AUTO_INCREMENT,
+    `nom` varchar (50) NOT NULL,
+    `prenom` varchar (50) NOT NULL,
+    `email` varchar (50) NOT NULL,
+    PRIMARY KEY (`id_client`)
+);
+
+DROP TABLE IF EXISTS `Tarif`;
+CREATE TABLE IF NOT EXISTS `Tarif` (
+    `id_tarif` int (11) NOT NULL AUTO_INCREMENT,
+    `nom` varchar (50) NOT NULL,
+    `prix_unitaire` int (5) NOT NULL,
+    `description` varchar (200) NOT NULL,
+    PRIMARY KEY (`id_tarif`)
+);
+
+DROP TABLE IF EXISTS `Film`;
+CREATE TABLE IF NOT EXISTS `Film` (
+    `id_film` int (11) NOT NULL AUTO_INCREMENT,
+    `titre` varchar (50) NOT NULL,
+    `annee_sortie` varchar (11) NOT NULL,
+    `description` varchar (200) NOT NULL,
+    PRIMARY KEY (`id_film`)
+);
+
+DROP TABLE IF EXISTS `Salle`;
+CREATE TABLE IF NOT EXISTS `Salle` (
+    `id_salle` int (11) NOT NULL AUTO_INCREMENT,
+    `nom` varchar (50) NOT NULL,
+    `nb_place_salle` int (11) NOT NULL,
+    `ref_film` int (11) NOT NULL,
+    PRIMARY KEY (`id_salle`)
+);
+
+DROP TABLE IF EXISTS `Reservation`;
+CREATE TABLE IF NOT EXISTS `Reservation` (
+    `id_reservation` int (11) NOT NULL AUTO_INCREMENT,
+    `nb_place_reservation` int (11) NOT NULL,
+    `moyen_paiement` varchar (50) NOT NULL,
+    `date_reservation` varchar (11) NOT NULL,
+    `ref_client` int (11) NOT NULL,
+    `ref_salle` int (11) NOT NULL,
+    `ref_tarif` int (11) NOT NULL,
+    PRIMARY KEY (`id_reservation`)
+);
+
+ALTER TABLE `Salle`
+    ADD CONSTRAINT `fk_salle_film` FOREIGN KEY (`ref_film`) REFERENCES `Film` (`id_film`);
+
+ALTER TABLE `Reservation`
+    ADD CONSTRAINT `fk_reservation_client` FOREIGN  KEY (`ref_client`) REFERENCES `Client` (`id_client`),
+    ADD CONSTRAINT `fk_reservation_salle` FOREIGN KEY (`ref_salle`) REFERENCES `Salle` (`id_salle`),
+    ADD CONSTRAINT `fk_reservation_tarif` FOREIGN KEY (`ref_tarif`) REFERENCES `Tarif` (`id_tarif`);
