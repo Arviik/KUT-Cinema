6c0316c36b8c46e82e440fe7cbd12e5b5afd55e9
Arviik <u.bercegeay@lprs.fr>
2021-11-29 17:25:59 +0100

    Ajout reservation.php et modif connexion.php

---
+++ Site/js/reservation.php
@@ -0,0 +1,14 @@
+<?php
+$date = \DateTime())->format('Y-m-d H:i:s');
+session_start();
+$bdd = new PDO('mysql:host=localhost;dbname=kut-cinema;charset=utf8','root','');
+$req = $bdd->prepare('INSERT INTO reservation (nb_place_reservation,moyen_paiement,date_reservation,ref_client,ref_salle,ref_tarif) VALUES (:nb_place_reservation,:moyen_paiement,:date_reservation,:ref_client,:ref_salle,:ref_tarif);');
+$req->execute(array(
+    "nb_place_reservation"=>$_POST['nb_place'],
+    "moyen_paiement"=>$_POST['moyen_paiement'],
+    "date_reservation"=>$date,
+    "ref_client"=>$_SESSION['id_client'],
+    "ref_salle"=>$_POST['salle'],
+    "ref_tarif"=>$_POST['tarif'],
+));
+header('Location: index.php');
