fbc32602d474dcf76aabf330ca92dc3f10b8a607
MAIGNAN_Ky <k.maignan@lprs.fr>
2021-12-01 09:18:09 +0100

    Modification de la navbar et de l'Index (non fini)

--- Site/connexion.php
+++ Site/connexion.php
@@ -11,7 +11,7 @@ if ($res){
     $_SESSION['nom'] = $res['nom'];
     $_SESSION['password'] = $res['mot_de_passe'];
     $_SESSION['id_client'] = $res['id_client'];
-    header('Location: index.php');
+    header('Location: modification.php');
 }
 else{
     header('Location: connexion.html');
