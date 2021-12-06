82ca072880f77516797be537dddb4c072d6b14d2
MAIGNAN_Ky <k.maignan@lprs.fr>
2021-12-06 17:25:06 +0100

    -Modification et ajout de ajoutsalle.html,ajoutfilm.html et affiche.php

--- Site/affiche.php
+++ Site/affiche.php
@@ -22,7 +22,9 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top py-1" id="mainNav">
     <div class="container px-4 px-lg-5">
-        <img href="index.php" src="assets/img/LogoKUTCinéma.png" height="75" alt="Logo de KUT cinéma">
+        <a href="index.php">
+            <img href="index.php" src="assets/img/LogoKUTCinéma.png" height="75" alt="Logo de KUT cinéma">
+        </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto my-2 my-lg-0">
