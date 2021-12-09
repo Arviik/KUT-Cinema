3bd8bcba1703d34bd5e8b57b3ea5df03d65a5e75
Arvik <ugo.bercegeay@gmail.com>
2021-12-10 00:25:58 +0100

    Finalisation de la gestion des salles, changement dans la navbar et le footer puis début de index_admin.php

--- Site/salle/update_salle.php
+++ Site/salle/update_salle.php
@@ -26,24 +26,22 @@
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                        <li  class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#portfolio">Films</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/Site/index.php">Accueil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../film/listefilm.php">Films</a></li>
                         <?php
                         if (isset($_SESSION['id_client'])){
-                            echo "<li class='nav-item'><a class='nav-link' href='/Site/compte/connexion.html'>Bienvenue " .$_SESSION['prenom']. "</a></li>
-                                              <li class='nav-item'><a class='nav-link' href='/Site/compte/modification.php'>Modifier profil</a></li>
-                                              <li class='nav-item'><a class='nav-link' href='/Site/compte/deconnexion.php'>Déconnexion</a></li>";
+                            echo "<li class='nav-item'><a class='nav-link' href='../compte/modification.php'>Bienvenue ".$_SESSION['prenom']."</a></li>
+                                          <li class='nav-item'><a class='nav-link' href='../compte/deconnexion.php'>Déconnexion</a></li>";
                         }else{
-                            echo "<li class='nav-item'><a class='nav-link' href='../compte/connexion.html'>Connexion</a></li>
-                                              <li class='nav-item'><a class='nav-link' href='../compte/inscription.html'>Inscription</a></li>";
+                            echo "<li class='nav-item'><a class='nav-link' href='../compte/inscription.html'>Inscription</a></li>
+                                          <li class='nav-item'><a class='nav-link' href='../compte/connexion.html'>Connexion</a></li>";
                         }
                         ?>
                     </ul>
                 </div>
             </div>
         </nav>
-        <section class="page-section pb-0" id="contact">
+        <section class="page-section pb-0" id="update_salle">
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8 col-xl-6 text-center">
@@ -54,20 +52,42 @@
                 </div>
                 <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
                     <div class="col-lg-6">
-                        <form action="updateDB.php" method="post">
+                        <form action="update_salle_DB.php" method="post">
                             <div class="form-floating mb-3">
                                 <input class="form-control" id="nom" type="text" value="<?php echo $res1['nom'] ?>" placeholder="Nom" data-sb-validations="required" name="nom" />
                                 <label for="nom">Nom</label>
                                 <div class="invalid-feedback" data-sb-feedback="name:required">Un nom est demander</div>
                             </div>
                             <div class="form-floating mb-3">
-                                <input class="form-control" id="nb_place_salle" type="text" value="<?php echo $res1['nb_place_salle'] ?>" placeholder="Nombre de place" data-sb-validations="required" name="nb_place_salle" />
+                                <select class="form-control" name="nb_place_salle" aria-label=".form-select-sm">
+                                    <?php
+                                        for ($i = 1; $i < 101; $i++){
+                                            if ($i == $res1['nb_place_salle']){
+                                                echo "<option value='".$i."' selected size='1'>".$i."</option>";
+                                            }else{
+                                                echo "<option value='".$i."' size='1'>".$i."</option>";
+                                            }
+                                        }
+                                    ?>
+                                </select>
                                 <label for="nb_place_salle">Nombre de place</label>
-                                <div class="invalid-feedback" data-sb-feedback="name:required">Le nombre de place est demander</div>
                             </div>
                             <div class="form-floating mb-3">
-                                <input class="form-control" id="ref_film" type="text" value="<?php echo $res1['ref_film'] ?>" placeholder="Film diffusé" name="ref_film" />
-                                <label for="ref_film">id du Film diffusé</label>
+                                <select class="form-control" name="ref_film" aria-label=".form-select-sm">
+                                    <?php
+                                        $req = $bdd->prepare('SELECT id_film, titre FROM film');
+                                        $req->execute();
+                                        $res2 = $req->fetchALL();
+                                        foreach ($res2 as $val){
+                                            if ($val['id_film'] == $res1['ref_film']){
+                                                echo "<option value='".$val['id_film']."' selected>".$val['titre']."</option>";
+                                            }else{
+                                                echo "<option value='".$val['id_film']."'>".$val['titre']."</option>";
+                                            }
+                                        }
+                                    ?>
+                                </select>
+                                <label for="ref_film">Film diffusé</label>
                             </div>
                             <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
                             <div class="d-grid"><button class="btn btn-primary btn-xl " id="submitButton" type="submit">Modifier la salle</button></div>
@@ -84,9 +104,7 @@
         </section>
         <footer class="bg-light py-5">
             <div class="d-flex justify-content-evenly">
-                <a href="../film/add_film.html" class="small text-muted">Ajout film</a>
                 <div class="small text-center text-muted">Copyright &copy; 2021 - KUT Cinéma</div>
-                <a href="../salle/add_salle.html" class="small text-muted">Ajout salle</a>
             </div>
         </footer>
     </body>
