49859b6fd3fc624cb8e84cf127046fe1215776cb
Shokkzu <shokkzu@gmail.com>
2021-12-11 22:03:12 +0100

    -Sécurisation de la suppréssion -Ajout du logo suppression

--- Site/reservation/delete_reservation.php
+++ Site/reservation/delete_reservation.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=kut-cinema;charset=utf8','root','');
-$req = $bdd->prepare('DElETE FROM reservation WHERE id_reservation=:id_reservation');
+$req = $bdd->prepare('DElETE FROM reservation WHERE id_reservation=:id_reservation AND ref_client=:id_client');
 $req->execute(array(
     "id_reservation"=>$_POST['id_reservation'],
+    "id_client"=>$_SESSION['id_client']
 ));
 header('Location: historiquereservation.php');
