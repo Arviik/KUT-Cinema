50984a96fb35399792f7a63026c14458b83ac482
Shokkzu <shokkzu@gmail.com>
2021-12-11 22:54:52 +0100

    Modifation de l'affiche de venom

--- Site/film/listefilm.php
+++ Site/film/listefilm.php
@@ -21,28 +21,29 @@
         <link href="../css/styles.css" rel="stylesheet"/>
     </head>
     <body id="page-top">
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-1" id="mainNavListe">
-            <div class="container px-4 px-lg-5">
-                <a href="../index.php">
-                    <img href="/Site/index.php" src="../assets/img/LogoKUTCinéma.png" height="75" alt="Logo de KUT cinéma">
-                </a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="../index.php">Accueil</a></li>
-                        <li class="nav-item"><a class="nav-link" href="../film/listefilm.php">Films</a></li>
-                        <?php
-                        if (isset($_SESSION['id_client'])){
-                            echo "<li class='nav-item'><a class='nav-link' href='../compte/modification.php'>Bienvenue ".$_SESSION['prenom']."</a></li>
-                                      <li class='nav-item'><a class='nav-link' href='../compte/deconnexion.php'>Déconnexion</a></li>";
-                        }else{
-                            echo "<li class='nav-item'><a class='nav-link' href='../compte/inscription.html'>Inscription</a></li>
-                                      <li class='nav-item'><a class='nav-link' href='../compte/connexion.html'>Connexion</a></li>";
-                        }?>
-                    </ul>
-                </div>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-1" id="mainNavListe">
+        <div class="container px-4 px-lg-5">
+            <a href="../index.php">
+                <img href="index.php" src="../assets/img/LogoKUTCinéma.png" height="75" alt="Logo de KUT cinéma">
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="../index.php">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../film/listefilm.php">Films</a></li>
+                    <?php
+                    if (isset($_SESSION['id_client'])){
+                        echo "<li class='nav-item'><a class='nav-link' href='../compte/modification.php'>Bienvenue ".$_SESSION['prenom']."</a></li>
+                                  <li class='nav-item'><a class='nav-link' href='../reservation/historiquereservation.php'>Reservation</a></li>
+                                  <li class='nav-item'><a class='nav-link' href='../compte/deconnexion.php'>Déconnexion</a></li>";
+                    }else{
+                        echo "<li class='nav-item'><a class='nav-link' href='../compte/inscription.html'>Inscription</a></li>
+                                  <li class='nav-item'><a class='nav-link' href='../compte/connexion.html'>Connexion</a></li>";
+                    }?>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
         <section class="page-section">
             <div class="container">
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
--- Site/reservation/historiquereservation.php
+++ Site/reservation/historiquereservation.php
@@ -8,30 +8,30 @@
         <link href="../css/styles.css" rel="stylesheet" />
         <link rel="icon" type="image/x-icon" href="../assets/img/LogoKUTCinéma.png"/>
     </head>
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-1" id="mainNavListe">
-            <div class="container px-4 px-lg-5">
-                <a href="../index.php">
-                    <img href="index.php" src="../assets/img/LogoKUTCinéma.png" height="75" alt="Logo de KUT cinéma">
-                </a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="../index.php">Accueil</a></li>
-                        <li class="nav-item"><a class="nav-link" href="../film/listefilm.php">Films</a></li>
-                        <?php
-                        if (isset($_SESSION['id_client'])){
-                            echo "<li class='nav-item'><a class='nav-link' href='../compte/modification.php'>Bienvenue ".$_SESSION['prenom']."</a></li>
-                                  <li class='nav-item'><a class='nav-link' href='reservation/historiquereservation.php'>Reservation</a></li>
+    <body id="page-top">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-1" id="mainNavListe">
+        <div class="container px-4 px-lg-5">
+            <a href="../index.php">
+                <img href="index.php" src="../assets/img/LogoKUTCinéma.png" height="75" alt="Logo de KUT cinéma">
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="../index.php">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../film/listefilm.php">Films</a></li>
+                    <?php
+                    if (isset($_SESSION['id_client'])){
+                        echo "<li class='nav-item'><a class='nav-link' href='../compte/modification.php'>Bienvenue ".$_SESSION['prenom']."</a></li>
+                                  <li class='nav-item'><a class='nav-link' href='../reservation/historiquereservation.php'>Reservation</a></li>
                                   <li class='nav-item'><a class='nav-link' href='../compte/deconnexion.php'>Déconnexion</a></li>";
-                        }else{
-                            echo "<li class='nav-item'><a class='nav-link' href='../compte/inscription.html'>Inscription</a></li>
+                    }else{
+                        echo "<li class='nav-item'><a class='nav-link' href='../compte/inscription.html'>Inscription</a></li>
                                   <li class='nav-item'><a class='nav-link' href='../compte/connexion.html'>Connexion</a></li>";
-                        }?>
-                    </ul>
-                </div>
-        </nav>
-
+                    }?>
+                </ul>
+            </div>
+        </div>
+    </nav>
         <section class="page-section">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-lg-8 col-xl-6 text-center">
