9ac251c3d11cbf5575c0ac609a68d9a74d7b888e
Astralale <benzekkitalal@gmail.fr>
2021-12-11 11:56:00 +0100

    test page ajout film

--- Site/film/add_film.php
+++ Site/film/add_film.php
@@ -1,7 +1,6 @@
 <?php
-session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=kut-cinema;charset=utf8','root','');
-$req = $bdd->prepare('INSERT INTO film (titre, annee_sortie, duree, genre, realisateur, acteur, description, image_link, image_bg) VALUES (:titre, :annee_sortie, :duree, :genre, :realisateur, :acteur, :description, :image_link, :image_bg)');
+$req = $bdd->prepare('INSERT INTO film (titre, annee_sortie, duree, genre, realisateur, acteur, description, image_link, image_bg) VALUES (:titre, :annee_sortie, :duree, :genre, :realisateur, :acteur, :description, :image_link, :image_bg);');
 $req->execute(array(
     "titre"=>$_POST['titre'],
     "annee_sortie"=>$_POST['annee_sortie'],
@@ -10,7 +9,7 @@ $req->execute(array(
     "realisateur"=>$_POST['realisateur'],
     "acteur"=>$_POST['acteur'],
     "description"=>$_POST['description'],
-    "image_link"=>"../assets/img/affiche/default_film.png",
-    "image_bg"=>"../assets/img/image_bg/bg-default_film.png"
+    "image_link"=>"dgfs",
+    "image_bg"=>"zdfza",
 ));
 header('Location: ../index.php');
